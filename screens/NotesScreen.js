31be632a625d30273c67fadae4389ad7baaf1341
rktmeister <dylan.raharja@gmail.com>
2021-06-11 18:51:47 +0800

    main

--- screens/NotesScreen.js
+++ screens/NotesScreen.js
@@ -5,8 +5,12 @@ import {
   View,
   TouchableOpacity,
   FlatList,
+  Button,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import firebase from "../database/firebaseDB";
+
+const db = firebase.firestore().collection("todos");
 
 export default function NotesScreen({ navigation, route }) {
   const [notes, setNotes] = useState([]);
@@ -27,6 +31,14 @@ export default function NotesScreen({ navigation, route }) {
           />
         </TouchableOpacity>
       ),
+      headerLeft: () => (
+        <Button
+          onPress={() => {
+            navigation.navigate("Login");
+          }}
+          title="Login"
+        />
+      ),
     });
   });
 
@@ -36,11 +48,27 @@ export default function NotesScreen({ navigation, route }) {
       const newNote = {
         title: route.params.text,
         done: false,
-        id: notes.length.toString(),
       };
-      setNotes([...notes, newNote]);
+      db.add(newNote);
     }
   }, [route.params?.text]);
+
+  // Load Firebase data on start
+  useEffect(() => {
+    const unsubscribe = db.onSnapshot((collection) => {
+      const updatedNotes = collection.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+      setNotes(updatedNotes);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   function addNote() {
     navigation.navigate("Add Screen");
@@ -50,7 +78,7 @@ export default function NotesScreen({ navigation, route }) {
   function deleteNote(id) {
     console.log("Deleting " + id);
     // To delete that item, we filter out the item we don't want
-    setNotes(notes.filter((item) => item.id !== id));
+    db.doc(id).delete();
   }
 
   // The function to render each row in our FlatList
